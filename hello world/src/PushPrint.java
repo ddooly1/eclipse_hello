11991dfd3bdd80cc3d3d4d69f83f51f91e3dd5c6
shinminho1 <34118319+shinminho1@users.noreply.github.com>
2017-11-30 15:09:32 +0900

    modified on github

--- hello world/src/PushPrint.java
+++ hello world/src/PushPrint.java
@@ -4,6 +4,7 @@ public class PushPrint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("add java class flie push");
+		System.out.println("hello world 365");
 	}
 
 }
